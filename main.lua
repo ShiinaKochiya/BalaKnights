48e31c2f90b9b45892cbb20c26cf302986d7f3a3
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-05-05 22:16:53 +0700

    Fix for Amiya

--- main.lua
+++ main.lua
@@ -25,7 +25,7 @@ SMODS.Joker{
             '{C:chips}+60{} Chips. This Donke gain',
             '{C:chips}+20{} Chips per Kings discarded.',
             "If the first discarded hand",
-            'only contains Kings of [suit],',
+            'only contains a single King,',
             "add a permanent copy of that",
             "card and draw into hand",
             "{C:inactive, C:grey}(Currently {C:chips}+#1#{C:inactive} Chips)"
@@ -47,6 +47,41 @@ SMODS.Joker{
     end,
 
     calculate = function(self,card,context)
+
+        if context.discard then
+            
+            if context.other_card:get_id() == 13 then
+                if G.GAME.current_round.discards_used <= 0 and #context.full_hand == 1 then
+                        card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.achip
+                        G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+                        local _card = copy_card(context.full_hand[1], nil, nil, G.playing_card)
+                        _card:add_to_deck()
+                        G.deck.config.card_limit = G.deck.config.card_limit + 1
+                        table.insert(G.playing_cards, _card)
+                        G.hand:emplace(_card)
+                        _card.states.visible = nil
+
+                        G.E_MANAGER:add_event(Event({
+                            func = function()
+                                _card:start_materialize()
+                                return true
+                            end
+                        })) 
+                        
+                        return{
+                            message = "Re-live"
+                        }
+                end
+
+                card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.achip
+                return{
+                    message = "Grow"
+                }
+
+            end
+            print(context.other_card)
+        end
+
         if context.joker_main then
         return{
             card = card,
