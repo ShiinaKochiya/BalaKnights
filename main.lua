5f9aa7155f172df4b125dfa1a5b65a301d9c05a6
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-05-05 22:43:34 +0700

    Debug deck
    Cos me lazy to Collection -> Move left -> 3 -> Escape every time I save something

--- main.lua
+++ main.lua
@@ -254,6 +254,63 @@ SMODS.Back {
     end
 }
 
+SMODS.Back {
+	key = "debug",
+	atlas = "Jokers",
+    loc_txt = {
+        name = 'Debug Deck',
+        text = {
+            "Same as RI Deck but also add Theresa and Amiya"
+        }
+    },
+    loc_args = {localize{type = 'name_text', key = 'v_magic_trick', set = 'Voucher'}, localize{type = 'name_text', key = 'v_illusion', set = 'Voucher'}},
+	pos = { x = 3, y = 0},
+    apply = function(self)
+        G.E_MANAGER:add_event(Event({
+            func = function()
+            	local newcards = {}
+                for i = 1, #G.playing_cards do
+      				local card = G.playing_cards[i]
+                    if card:get_id() == 12 or card:get_id() == 13 or card:get_id() == 11 then
+                        local _card = copy_card(card, nil, nil, G.playing_card)
+                        _card:add_to_deck()
+                        G.deck.config.card_limit = G.deck.config.card_limit + 1
+                        table.insert(G.playing_cards, _card)
+                        table.insert(G.playing_cards, _card)
+                        G.deck:emplace(_card)
+                    end
+                end
+                
+                local card = SMODS.create_card({
+                    set = 'Joker',
+                    area = G.jokers,
+                    key = 'j_blk_prts',
+                    stickers = { 'eternal' },
+                })
+                card:set_edition({negative = true}, true)
+                card:add_to_deck()
+                G.jokers:emplace(card)
+
+                local card = SMODS.create_card({
+                    set = 'Joker',
+                    area = G.jokers,
+                    key = 'j_blk_amiyi',
+                })
+                card:add_to_deck()
+                G.jokers:emplace(card)
+
+                local card = SMODS.create_card({
+                    set = 'Joker',
+                    area = G.jokers,
+                    key = 'j_blk_theresa',
+                })
+                card:add_to_deck()
+                G.jokers:emplace(card)
+                return true
+            end
+        }))
+    end
+}
 
 function values(t)
     local i = 0
