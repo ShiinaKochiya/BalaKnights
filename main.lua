e52316b0b3e26e352a93de12acfe980fe6003513
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-05-09 21:25:37 +0700

    messing around with some new concept

--- main.lua
+++ main.lua
@@ -212,82 +212,37 @@ SMODS.Joker{
 }
 
 -- Joker: Mr.Cannot
---[[
 SMODS.Joker{
-    key = 'amiyi',
+    key = 'cannot',
     loc_txt = {
-        name = 'Amiya',
+        name = 'Terra Investment Masterclass',
         text = {
-            '{C:chips}+60{} Chips. This Donke gain',
-            '{C:chips}+20{} Chips per Kings discarded.',
-            "If the first discarded hand",
-            'only contains a single King,',
-            "add a permanent copy of that",
-            "card and draw into hand",
-            "{C:inactive, C:grey}(Currently {C:chips}+#1#{C:inactive} Chips)"
+            'Testing',
         }
     },
     atlas = 'Jokers',
-    pos = { x=0, y=0 },
+    pos = { x=4, y=0 },
     config = {
         extra = {
             chips = 60,
             achip = 20,
+            odds = 2,
         }
     },
     loc_vars = function(self,info_queue,center)
         return {vars = {
-            center.ability.extra.chips,
-            center.ability.extra.achips
         }}
     end,
 
     calculate = function(self,card,context)
-
-        if context.discard then
-            
-            if context.other_card:get_id() == 13 then
-                if G.GAME.current_round.discards_used <= 0 and #context.full_hand == 1 then
-                        card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.achip
-                        G.playing_card = (G.playing_card and G.playing_card + 1) or 1
-                        local _card = copy_card(context.full_hand[1], nil, nil, G.playing_card)
-                        _card:add_to_deck()
-                        G.deck.config.card_limit = G.deck.config.card_limit + 1
-                        table.insert(G.playing_cards, _card)
-                        G.hand:emplace(_card)
-                        _card.states.visible = nil
-
-                        G.E_MANAGER:add_event(Event({
-                            func = function()
-                                _card:start_materialize()
-                                return true
-                            end
-                        })) 
-                        
-                        return{
-                            message = "Re-live"
-                        }
-                end
-
-                card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.achip
-                return{
-                    message = "Grow"
-                }
-
-            end
-            --print(context.other_card)
+        if context.joker_main and pseudorandom('terra') < G.GAME.probabilities.normal/card.ability.extra.odds then
+            return {
+                message = "Test"
+            }
         end
-
-        if context.joker_main then
-        return{
-            card = card,
-            chips = card.ability.extra.chips,
-            colour = G.C.CHIPS
-        }
-        end
+    --end of calc funct
     end
 }
---]]
 
 SMODS.Back {
 	key = "rhodes",
